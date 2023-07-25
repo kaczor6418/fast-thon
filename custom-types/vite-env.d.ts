60cfdef85482a76675db5f1e4f00f388518c47c1
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2023-07-25 19:17:02 +0200

    feat: add firebase hosting deploy flow

--- custom-types/vite-env.d.ts
+++ custom-types/vite-env.d.ts
@@ -10,7 +10,7 @@ interface ImportMetaEnv extends BuiltInEnvVariables {
   readonly VITE_FIREBASE_API_KEY: string;
   readonly VITE_FIREBASE_APP_ID: string;
   readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-  readonly VITE_FIREBASE_MEASUREMENT_ID: string;
+  readonly VITE_DATABASE_URL: string;
   readonly VITE_FIREBASE_MESSAGING_ID: string;
   readonly VITE_FIREBASE_PROJECT_ID: string;
   readonly VITE_FIREBASE_STORAGE_BUCKET: string;
