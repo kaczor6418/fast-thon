6befe959c86ecbb6c1513eda58dcd7f32383480d
Krzysztof Kaczyński <34687242+kaczor6418@users.noreply.github.com>
2023-07-26 23:06:13 +0200

    feat: set-up ci/cd for PR and merge (#3)
    * feat: add firebase hosting deploy flow
    
    * feat: set-up ci/cd for PR and merge
    
    * fix: prettier issues and update ingored files list

--- src/modules/firebase-module/firebase.config.ts
+++ src/modules/firebase-module/firebase.config.ts
@@ -7,7 +7,7 @@ export const firebaseApp = initializeApp({
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
