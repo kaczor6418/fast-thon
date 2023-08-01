5a7bbcaaf8bf2a10d7d3313b5d1b6ae6f932355c
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2023-08-02 01:00:18 +0200

    fix: regExp matcher + prettyprint mjs

--- jest.config.mjs
+++ jest.config.mjs
@@ -3,6 +3,7 @@
  * https://jestjs.io/docs/configuration
  */
 
+// eslint-disable-next-line import/no-default-export
 export default {
   // All imported modules in your tests should be mocked automatically
   // automock: false,
@@ -11,7 +12,7 @@ export default {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  cacheDirectory: "jest_cache",
+  cacheDirectory: 'jest_cache',
 
   // Automatically clear mock calls, instances, contexts and results before every test
   // clearMocks: false,
@@ -23,15 +24,13 @@ export default {
   // collectCoverageFrom: undefined,
 
   // The directory where Jest should output its coverage files
-  coverageDirectory: "/coverage",
+  coverageDirectory: '/coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  coveragePathIgnorePatterns: ['/node_modules/'],
 
   // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: "v8",
+  coverageProvider: 'v8',
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
@@ -144,7 +143,7 @@ export default {
   // snapshotSerializers: [],
 
   // The test environment that will be used for testing
-  testEnvironment: "jsdom",
+  testEnvironment: 'jsdom',
 
   // Options that will be passed to the testEnvironment
   // testEnvironmentOptions: {},
@@ -153,14 +152,10 @@ export default {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/test/**/*spec.ts?(x)"
-  ],
+  testMatch: ['**/test/**/*spec.ts?(x)'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  testPathIgnorePatterns: ['/node_modules/'],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -173,8 +168,8 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    "^.+\\.(t|j)sx?$": "@swc/jest"
-  }
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
