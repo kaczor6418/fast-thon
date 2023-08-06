50b686612692dcba3ec8821afdd1fb7167cf6449
Krzysztof Kaczyński <k.kaczynski97@gmail.com>
2023-08-06 22:18:43 +0200

    perf: run git-hhoks checks in parallel

---
+++ git-hooks/run-firebase-security-rules-tests.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+source ./git-hooks/git-hooks-helpers.sh
+source ./git-hooks/logs-colors.sh
+
+TESTS_EXIT_CODE=0
+EXTENSIONS_TO_CHECK=".*\.rules$"
+STAGED_FILES=$(git diff --cached --diff-filter=d --name-only | grep -E "$EXTENSIONS_TO_CHECK")
+
+if [[ -n "$STAGED_FILES" ]]; then
+  echo -e "${BLUE} 🔥 Running Firebase security rules tests... ${NC}"
+  ./node_modules/.bin/jest /test/integration --coverage=false --testEnvironment=node --passWithNoTests --setupFilesAfterEnv ./test/integration/integration-setup.ts
+  TESTS_EXIT_CODE=$?
+  printf "\n"
+fi
+
+echo $TESTS_EXIT_CODE >"$FIREBASE_SECURITY_RULES_EXIT_CODE_TMP"
